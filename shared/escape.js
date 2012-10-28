e9624d0e2b1ee0be88df25d53dec963e172da80d
Chris Peterson <chris@ca-peterson.com>
2012-10-28 14:04:49 -0700

    Record data is HTML-escaped
    Fixed an issue where HTML data would be displayed as inline HTML. Now
    the literal source is shown.

---
+++ shared/escape.js
@@ -0,0 +1,17 @@
+/*Helpfully borrowed from https://gist.github.com/1795519#file_html_escape.js */
+
+// Use the browser's built-in functionality to quickly and safely escape the
+// string
+function escapeHtml(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
+
+// UNSAFE with unsafe strings; only use on previously-escaped ones!
+function unescapeHtml(escapedStr) {
+    var div = document.createElement('div');
+    div.innerHTML = escapedStr;
+    var child = div.childNodes[0];
+    return child ? child.nodeValue : '';
+};
