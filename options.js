86d0a20e622407de03af0cb9b24e275d9ad33822
Chris Peterson <chris@ca-peterson.com>
2012-12-27 17:16:59 -0800

    Added configuration screen to control metadata caching

---
+++ options.js
@@ -0,0 +1,26 @@
+var options = new Object();
+// Saves options to localStorage.
+function save_options() {
+  options.connection_cleanup_age = document.getElementById("connection_cleanup_age").value;
+  localStorage["connection_cleanup_age"] = options.connection_cleanup_age;
+
+  // Update status to let user know options were saved.
+  var status = document.getElementById("status");
+  status.innerHTML = "Options Saved.";
+  setTimeout(function() {
+    status.innerHTML = "";
+  }, 750);
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  
+  options.connection_cleanup_age = localStorage["connection_cleanup_age"];
+  document.getElementById("connection_cleanup_age").value = options.connection_cleanup_age;
+  document.getElementById('save').addEventListener('click', save_options);
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+  restore_options();
+})
+document.addEventListener('DOMContentReady', restore_options);
